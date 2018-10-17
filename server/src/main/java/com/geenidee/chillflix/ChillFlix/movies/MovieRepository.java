b7a3afd2a3eab97de9a1a4864ccdd4e02ec4cc99
jschuiteboer <jschuiteboer@users.noreply.github.com>
2018-10-17 18:18:28 +0200

    added rest movie repository via spring data rest

---
+++ server/src/main/java/com/geenidee/chillflix/ChillFlix/movies/MovieRepository.java
@@ -0,0 +1,6 @@
+package com.geenidee.chillflix.ChillFlix.movies;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface MovieRepository extends PagingAndSortingRepository<Movie, Long> {
+}
