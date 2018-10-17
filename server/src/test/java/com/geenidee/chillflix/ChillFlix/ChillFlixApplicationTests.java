c15fe4f55898d96681997ba813ccf60976f38bfa
jschuiteboer <jschuiteboer@users.noreply.github.com>
2018-10-17 16:35:44 +0200

    added spring boot application for server backend

---
+++ server/src/test/java/com/geenidee/chillflix/ChillFlix/ChillFlixApplicationTests.java
@@ -0,0 +1,16 @@
+package com.geenidee.chillflix.ChillFlix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChillFlixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
