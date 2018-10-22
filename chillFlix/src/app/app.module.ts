41b61431ac83954ce1c14ddeb36774ce7d951510
BasvBo <37073686+BasvBo@users.noreply.github.com>
2018-10-22 11:04:40 +0200

    Master Pull

--- chillFlix/src/app/app.module.ts
+++ chillFlix/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
-import{ AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    MovieListComponent
-  ],
+  declarations: [AppComponent,HomeComponent, MovieListComponent],
   imports: [BrowserModule, AppRoutingModule  ],
   providers: [ ],
   bootstrap: [AppComponent]
