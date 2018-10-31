d06095f4f9617bdbb4d320478190da2d337c8355
BasvBo <37073686+BasvBo@users.noreply.github.com>
2018-10-31 21:27:28 +0100

    smal tweax

---
+++ chillFlix/src/app/guards/admin-auth-guard.service.ts
@@ -0,0 +1,11 @@
+
+import { Injectable } from '@angular/core';
+import{CanActivate, ActivatedRouteSnapshot}from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminAuthGuardService {
+
+  constructor() { }
+}
