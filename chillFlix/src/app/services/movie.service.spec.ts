2386071a12a5c98da393878c5be845f41b7c95f9
jschuiteboer <jschuiteboer@users.noreply.github.com>
2018-12-04 21:17:18 +0100

    fix movie service test

--- chillFlix/src/app/services/movie.service.spec.ts
+++ chillFlix/src/app/services/movie.service.spec.ts
@@ -2,8 +2,14 @@ import { TestBed } from '@angular/core/testing';
 
 import { MovieService } from './movie.service';
 
+const movieServiceMock = {};
+
 describe('FilmsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: MovieService, useValue: movieServiceMock }
+    ]
+  }));
 
   it('should be created', () => {
     const service: MovieService = TestBed.get(MovieService);
