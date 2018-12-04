a975173d68720f914f3e9ce23d5b84ce0678855e
jschuiteboer <jschuiteboer@users.noreply.github.com>
2018-12-04 20:28:25 +0100

    fix app component test

--- chillFlix/src/app/app.component.spec.ts
+++ chillFlix/src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from "@angular/router/testing";
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule, routingComponents } from "./app-routing.module";
+import { SafePipe } from "./pipes/SafePipe";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        AppRoutingModule,
+        RouterTestingModule.withRoutes([]),
+      ],
       declarations: [
-        AppComponent
+        AppComponent,
+        routingComponents,
+        SafePipe,
       ],
     }).compileComponents();
   }));
@@ -16,16 +26,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'chillFlix'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('chillFlix');
-  });
-
-  it('should render title in a h1 tag', () => {
+  it('should have a navbar-brand containing ChillFlix', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to chillFlix!');
+    expect(compiled.querySelector('.navbar-brand').textContent).toContain('ChillFlix');
   });
 });
